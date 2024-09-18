1a2fae26d251d544cd31bf8e6326485970a1bdf8
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-09-18 11:00:27 +0530

    Fixed bug in src/TargetInArray.java file

--- src/TargetInArray.java
+++ src/TargetInArray.java
@@ -6,23 +6,26 @@ public class TargetInArray {
 
 	public static void main(String[] args) {
 		int[] arr= new int[5];
+		int[][] pairs = new int[5][5];
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter an array values");
+		System.out.print("Enter an array values: ");
 		for(int i=0;i<arr.length;i++) {
 			arr[i]=scan.nextInt();
 		}
-		System.out.println("Enter the targe: ");
+		System.out.println("Enter the target: ");
 		int target=scan.nextInt();
 		for(int i=0;i<arr.length;i++) {
 			for(int j=arr.length-1;j>=0;j--) {
 				if(arr[i]+arr[j]==target) {
-					System.out.println(i+" "+j);
+					if(i!=j){
+						pairs[0][0]=i;
+						pairs[0][1]=j;
+					}
 					break; 
 				}
-				System.out.println();
 			}
-		} 
+		}
+		System.out.println(pairs[0][0]+" "+ pairs[0][1]);
 		scan.close();
 	}
-
 }
